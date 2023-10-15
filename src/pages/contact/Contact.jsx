e962631100ae498043bca997898059227b57f6fe
SabaG63 <sgach2005@gmail.com>
2023-10-15 23:56:39 +0400

    add contact section

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,96 @@
+import { Link } from "react-router-dom";
 export const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <section className="contact" id="contact">
+      <div className="contact-top">
+        <div className="contact-wrap">
+          <h1 className="contact-title">
+            <span className="contact-block"></span> CONTACT
+          </h1>
+        </div>
+      </div>
+
+      {/* start location-message-email section */}
+      <div className="contact-container">
+        <div className="contact-container-top">
+          <div className="contact-container-1 contact-wrapper-box">
+            <img
+              className="contact-icon location-icon"
+              src={require("../../images/location.png")}
+              alt="location-icon"
+            />
+            <h2 className="contact-info-text">LOCATION:</h2>
+            <p>Buxaidze street N17</p>
+          </div>
+          <div className="contact-container-2 contact-wrapper-box">
+            <img
+              className="contact-icon email-icon"
+              src={require("../../images/email-box.png")}
+              alt="email-icon"
+            />
+            <h2 className="contact-info-text">EMAIL US:</h2>
+            <p>fitfactory@gmail.com</p>
+          </div>
+          <div className="contact-container-3 contact-wrapper-box">
+            <img
+              className="contact-icon message-icon"
+              src={require("../../images/message-icon.png")}
+              alt="message-icon"
+            />
+            <h2 className="contact-info-text">CALL US:</h2>
+            <p>+(599) 599-09-08-09</p>
+          </div>
+        </div>
+      </div>
+      {/* end location-message-email section */}
+
+      <div className="contact-sign">
+        <div className="contact-sign-top">
+          <div className="contact-sign-left">
+            <img
+              className="contact-main-image"
+              src={require("../../images/contact-mainImg.png")}
+              alt="contact-imageBox"
+            />
+          </div>
+          <div className="contact-sign-right">
+            <h3>SEND US MESSAGE:</h3>
+            <div className="contact-flex">
+              <input
+                id="username"
+                type="text"
+                name="username"
+                placeholder="* Full Name"
+                required
+              />
+              <input
+                id="mail-input"
+                type="text"
+                name="email"
+                placeholder="* Your Email"
+                required
+              />
+              <input
+                id="TrainerSelector"
+                type="text"
+                name="username"
+                placeholder="* Select Trainer"
+                required
+              />
+              <textarea
+                placeholder="* Message"
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+              ></textarea>
+              <Link to="/start-trial" className="contact-form-btn">
+                SUBMIT
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
